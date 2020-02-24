20f128b67d384a7d9504ed9737f0f0e645a4851b
Riddle-Ghost <test@test.com>
2020-02-24 18:40:30 +0300

    add flash vue component

---
+++ resources/js/components/Flash.vue
@@ -0,0 +1,29 @@
+<template>
+<div v-if="flash" @click="unsetFlash" class="alert alert-primary" role="alert">
+    {{flash}}
+</div>
+</template>
+
+<script>
+export default {
+    computed: {
+        flash() {
+            return this.$store.getters.getFlash
+        }
+    },
+    methods: {
+        unsetFlash() {
+            this.$store.commit('changeFlash', null)
+        }
+    },
+    beforeDestroy() {
+        this.unsetFlash()
+    }
+}
+</script>
+
+<style>
+.alert {
+    cursor: pointer;
+}
+</style>
